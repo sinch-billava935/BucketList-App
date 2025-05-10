b697a0b1fb2652f93b38933475afb0920acdd7af
Sinchana T <sinchana7799@gmail.com>
2025-05-10 20:33:53 +0530

    new bucketList adding app using flutter commits

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:bucketlist/screens/add_screen.dart';
+import 'package:bucketlist/screens/main_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MainScreen(),
+      theme: ThemeData.light(useMaterial3: true),
+    );
+  }
+}
+//declaring the name of routes
+      //using named route
+      // routes: {
+      //   "/home": (context) {
+      //     return Mainscreen();
+      //   },
+      //   "/add": (context) {
+      //     return AddBucketListScreen();
+      //   },
+      // },
+      // initialRoute: "/home",
